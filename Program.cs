cc4a2f0fda78787a2148e74eb3367045eb4e1dbe
Mehmet Ali <153413232+mehmetalidurusoy@users.noreply.github.com>
2024-10-09 12:05:25 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ int uzunYadaYukseklik = 0;
 
 programBilgi();
 sekilSec();
-
 hesapla();
 
 void mesaj(string mesaj, ConsoleColor onrek = ConsoleColor.Gray)
@@ -45,10 +44,10 @@ double alanHesapla(int sekil)
 {
     switch (sekil)
     {
-        case 1: 
-          kisaYadaTaban = sGir(1,"Kısa Kenar");
-          uzunYadaYukseklik = sGir(2,"Uzun Kenar"); 
-          return kisaYadaTaban * uzunYadaYukseklik; 
+        case 1:
+            kisaYadaTaban = sGir(1, "Kısa Kenar");
+            uzunYadaYukseklik = sGir(2, "Uzun Kenar");
+            return kisaYadaTaban * uzunYadaYukseklik;
         break;
 
         case 2: alan = sGir(); return pi * alan * alan; break;
@@ -68,7 +67,7 @@ double cevreHesapla(int sekil)
         case 1: return (kisaYadaTaban * uzunYadaYukseklik) * 2; break;
         case 2: return 2 * pi * alan; break;
         case 3: return pi * alan * 2; break;
-        case 4: return kisaYadaTaban +  uzunYadaYukseklik; break;
+        case 4: return kisaYadaTaban + uzunYadaYukseklik; break;
         default: return 0; break;
     }
 }
@@ -77,7 +76,7 @@ void ekranaYaz(double alan, double cevre)
 {
     Console.Write($"Alan:  ");
     mesaj(Convert.ToString(alan), ConsoleColor.Green);
-    
+
     Console.Write($"Çevre: ");
     mesaj(Convert.ToString(cevre), ConsoleColor.Green);
 }
